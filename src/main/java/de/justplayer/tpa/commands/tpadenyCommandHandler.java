374881d0a5a6ab64fde144c373d5efc4da73a1ae
Justin K. <justin.k@justplayer.de>
2023-11-02 18:20:44 +0100

    add support for selected teleport deny

--- src/main/java/de/justplayer/tpa/commands/tpadenyCommandHandler.java
+++ src/main/java/de/justplayer/tpa/commands/tpadenyCommandHandler.java
@@ -28,14 +28,37 @@ public class tpadenyCommandHandler implements CommandExecutor {
             return true;
         }
 
+        if(args.length > 0) {
+            Player requestSender = plugin.getServer().getPlayer(args[0]);
+
+            if (requestSender == null) {
+                player.sendMessage(plugin.config.getString("messages.prefix") + "Player not found");
+                return true;
+            }
+
+            Request request = plugin.teleportRequestManager.getRequest(requestSender.getUniqueId(), player.getUniqueId());
+
+            if(request == null) {
+                player.sendMessage(plugin.config.getString("messages.prefix") + "You have no pending request from that player");
+                return true;
+            }
+
+            plugin.teleportRequestManager.cancelRequest(request, "Teleportation Denied by player");
+            player.sendMessage(plugin.config.getString("messages.prefix") + "You have denied the Teleportation request from " + requestSender.getName());
+            return true;
+        }
+
         List<Request> requests = plugin.teleportRequestManager.getRequestsForPlayer(player.getUniqueId());
         if (requests.isEmpty()) {
             player.sendMessage(plugin.config.getString("messages.prefix") + "You have no pending request");
             return true;
         }
 
-        // Cancel oldest request
+        Request request = requests.get(0);
+        Player requestSender = plugin.getServer().getPlayer(request.getSender());
+
         plugin.teleportRequestManager.cancelRequest(requests.get(0), "Teleportation Denied by player");
+        player.sendMessage(plugin.config.getString("messages.prefix") + "You have denied the Teleportation request from " + (requestSender != null ? requestSender.getName() : "Unknown Player"));
 
         return true;
     }

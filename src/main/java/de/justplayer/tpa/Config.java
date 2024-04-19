b64a8aed4bd0f23f8f599dea49705e299dc9afba
Justin K. <justin.k@justplayer.de>
2024-04-19 02:17:25 +0200

    add comments to config file

--- src/main/java/de/justplayer/tpa/Config.java
+++ src/main/java/de/justplayer/tpa/Config.java
@@ -2,6 +2,9 @@ package de.justplayer.tpa;
 
 import org.bukkit.configuration.file.FileConfiguration;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Config {
     private final Plugin plugin;
 
@@ -10,21 +13,36 @@ public class Config {
         this.plugin = plugin;
         FileConfiguration config = this.plugin.getConfig();
 
-
-        // Time in seconds you have to accept a teleport request
         config.addDefault("tpa.timeout", 60);
-        // Time in seconds you have to wait before you teleport (after teleport request was accepted)
-        // Any movement will cancel the teleport, 0 to disable (not implemented yet)
+        config.setComments("tpa.timeout", List.of("Time in seconds you have to accept a teleport request"));
+
         config.addDefault("tpa.wait", 5);
-        // Time in seconds you have to wait before you can send another teleport request
+        config.setComments("tpa.wait", List.of(
+                "Time in seconds you have to wait before you teleport (after teleport request was accepted)",
+                "Any movement will cancel the teleport, 0 to disable",
+                "Note: this feature is not implemented yet, teleports are currently instant."
+        ));
+
         config.addDefault("tpa.cooldowns.tpa", 60);
-        // Time in seconds you have to wait before you can send another tpa here request
+        config.setComments("tpa.cooldowns.tpa", List.of(
+                "Time in seconds you have to wait before you can send another teleport request"
+        ));
+
         config.addDefault("tpa.cooldowns.tpaHere", 60);
+        config.setComments("tpa.cooldowns.tpaHere", List.of(
+                "Time in seconds you have to wait before you can send another tpa here request"
+        ));
 
         config.addDefault("bStats.enabled", true);
+        config.setComments("bStats.enabled", List.of(
+                "Enable bStats for this plugin"
+        ));
 
         // We use modrinth to check for updates
         config.addDefault("check-for-updates", true);
+        config.setComments("check-for-updates", List.of(
+                "Check for updates on startup, this uses the modrinth api to get the newest version for your server."
+        ));
 
         config.addDefault("messages.prefix", "§8[§6JustTPA§8] §7");
         // TODO: Add more messages

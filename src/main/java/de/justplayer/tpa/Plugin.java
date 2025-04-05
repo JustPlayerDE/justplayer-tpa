779483078006d5fbc715f3e583529592cd845903
Justin K. <justin.k@justplayer.de>
2025-04-05 03:40:13 +0200

    we just do a little refactor of the plugin stuff

--- src/main/java/de/justplayer/tpa/Plugin.java
+++ src/main/java/de/justplayer/tpa/Plugin.java
@@ -4,12 +4,12 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import de.justplayer.tpa.commands.*;
-import de.justplayer.tpa.listeners.PlayerLeaveListener;
-import de.justplayer.tpa.listeners.PlayerMoveListener;
+import de.justplayer.tpa.listeners.*;
 import de.justplayer.tpa.utils.CooldownManager;
 import de.justplayer.tpa.utils.TeleportRequestManager;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.net.http.HttpClient;
@@ -31,13 +31,49 @@ public class Plugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        initialiseCommands();
 
-        // Register listeners
-        getServer().getPluginManager().registerEvents(new PlayerLeaveListener(this), this);
-        handleOptionalListeners();
+        initialiseEvents();
+        initialiseOptionalEvents();
 
+        teleportRequestManager.start();
 
-        // Register commands
+        initialiseStatistics();
+        checkForUpdates();
+
+        getLogger().info("JustTPA initialized");
+        if (getDescription().getVersion().contains("dev")) {
+            getLogger().warning("Using Development version, plugin may be unstable. bStats and Update checks are disabled.");
+        }
+    }
+
+    private void initialiseEvents() {
+        PluginManager pluginManager = getServer().getPluginManager();
+
+        pluginManager.registerEvents(new PlayerLeaveListener(this), this);
+    }
+
+    /**
+     * Enable or Disable specific (optional) listeners based on config
+     * I don't want these to run if they aren't used.
+     */
+    public void initialiseOptionalEvents() {
+        if (config == null) {
+            // We are still initialising
+            return;
+        }
+        PluginManager pluginManager = getServer().getPluginManager();
+
+        if (config.getInt("tpa.wait", 0) > 0) {
+            log("PlayerMoveEvent Listener registered.", "Debug");
+            pluginManager.registerEvents(new PlayerMoveListener(this), this);
+        } else {
+            log("PlayerMoveEvent Listener unregistered.", "Debug");
+            PlayerMoveEvent.getHandlerList().unregister(this);
+        }
+    }
+
+    private void initialiseCommands() {
         Objects.requireNonNull(getCommand("tpa")).setExecutor(new tpaCommandHandler(this));
         Objects.requireNonNull(getCommand("tpahere")).setExecutor(new tpahereCommandHandler(this));
         Objects.requireNonNull(getCommand("tpaccept")).setExecutor(new tpacceptCommandHandler(this));
@@ -45,69 +81,69 @@ public class Plugin extends JavaPlugin {
         Objects.requireNonNull(getCommand("tpacancel")).setExecutor(new tpacancelCommandHandler(this));
         Objects.requireNonNull(getCommand("tpareload")).setExecutor(new tpareloadCommandHandler(this));
         Objects.requireNonNull(getCommand("tpareturn")).setExecutor(new tpareturnCommandHandler(this));
-        // Start the teleport request manager scheduler
-        teleportRequestManager.start();
+    }
 
-        getLogger().info("JustTPA initialized");
-
-        if(getDescription().getVersion().contains("dev")) {
-            getLogger().warning("Using Development version, plugin may be unstable. bStats and Update checks are disabled.");
+    private void initialiseStatistics() {
+        if (!config.getBoolean("bStats.enabled") || getDescription().getVersion().contains("dev")) {
+            return;
         }
 
-        if (config.getBoolean("bStats.enabled") && !getDescription().getVersion().contains("dev")) {
-            new Metrics(this, 18743);
-            getLogger().info("bStats enabled");
+        new Metrics(this, 18743);
+        getLogger().info("bStats enabled");
+    }
+
+
+    private void checkForUpdates() {
+        if (!config.getBoolean("check-for-updates") || getDescription().getVersion().contains("dev")) {
+            return;
         }
 
-        /**
-         * Check for updates using modrinth
-         */
-        if (config.getBoolean("check-for-updates") && !getDescription().getVersion().contains("dev")) {
-            String minecraftVersion = getServer().getVersion().split("-")[0];
-            String serverSoftware = getServer().getVersion().split("-")[1].split(" ")[0].toLowerCase();
-            String currentPluginVersion = getDescription().getVersion();
+        String minecraftVersion = getServer().getVersion().split("-")[0];
+        String serverSoftware = getServer().getVersion().split("-")[1].split(" ")[0].toLowerCase();
+        String currentPluginVersion = getDescription().getVersion();
 
-            getLogger().info("Checking for updates...");
+        getLogger().info("Checking for updates...");
 
-            getServer().getScheduler().runTaskAsynchronously(this, () -> {
-                HttpClient client = HttpClient.newHttpClient();
+        getServer().getScheduler().runTaskAsynchronously(this, () -> {
+            HttpClient client = HttpClient.newHttpClient();
 
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(java.net.URI.create("https://api.modrinth.com/v2/project/justplayer-tpa/version?game_versions=" + minecraftVersion + "&loaders=" + serverSoftware))
-                        .GET()
-                        .header("User-Agent", "JustPlayerDE/justplayer-tpa/v" + currentPluginVersion + " (https://modrinth.com/plugin/justplayer-tpa justin.k@justplayer.de)")
-                        .build();
+            // The Modrinth api is very handy for this, we just provide the current context, and it returns if there is something new for that.
+            // With new I mean something different than we have currently, too lazy to implement actual version logic and just assume different = new ^^
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(java.net.URI.create("https://api.modrinth.com/v2/project/justplayer-tpa/version?game_versions=" + minecraftVersion + "&loaders=" + serverSoftware))
+                    .GET()
+                    .header("User-Agent", "JustPlayerDE/justplayer-tpa/v" + currentPluginVersion + " (https://modrinth.com/plugin/justplayer-tpa justin.k@justplayer.de)")
+                    .build();
 
-                try {
-                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                    if (response.statusCode() != 200) {
-                        getLogger().warning("Failed to check for updates");
-                        return;
-                    }
-
-                    JsonArray jsonResponse = JsonParser.parseString(response.body()).getAsJsonArray();
-
-                    if (jsonResponse.isEmpty()) {
-                        getLogger().warning("Failed to check for updates (no supported versions found)");
-                        return;
-                    }
-
-                    JsonObject latestVersion = jsonResponse.get(0).getAsJsonObject();
-                    String latestPluginVersion = latestVersion.get("version_number").getAsString();
-
-                    if (!currentPluginVersion.equals(latestPluginVersion)) {
-                        getLogger().info("A new version is available: " + latestPluginVersion);
-                        getLogger().info("Download it at: https://modrinth.com/plugin/justplayer-tpa/versions?l=" + serverSoftware + "&g=" + minecraftVersion);
-                    } else {
-                        getLogger().info("You are using the latest version for your server.");
-                    }
-
-                } catch (Exception e) {
+            try {
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() != 200) {
                     getLogger().warning("Failed to check for updates");
-                    e.printStackTrace();
+                    return;
                 }
-            });
-        }
+
+                JsonArray jsonResponse = JsonParser.parseString(response.body()).getAsJsonArray();
+
+                if (jsonResponse.isEmpty()) {
+                    getLogger().warning("Failed to check for updates (no supported versions found)");
+                    return;
+                }
+
+                JsonObject latestVersion = jsonResponse.get(0).getAsJsonObject();
+                String latestPluginVersion = latestVersion.get("version_number").getAsString();
+
+                if (!currentPluginVersion.equals(latestPluginVersion)) {
+                    getLogger().info("A new version is available: " + latestPluginVersion);
+                    getLogger().info("Download it at: https://modrinth.com/plugin/justplayer-tpa/versions?l=" + serverSoftware + "&g=" + minecraftVersion);
+                } else {
+                    getLogger().info("You are using the latest version for your server.");
+                }
+
+            } catch (Exception e) {
+                getLogger().warning("Failed to check for updates");
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
@@ -118,37 +154,14 @@ public class Plugin extends JavaPlugin {
     }
 
     @Override
-    public void reloadConfig()
-    {
+    public void reloadConfig() {
         super.reloadConfig();
-        this.handleOptionalListeners();
+        this.initialiseOptionalEvents();
     }
-
-    /**
-     * Enable or Disable specific (optional) listeners based on config
-     * I don't want these to run if they aren't used.
-     */
-    public void handleOptionalListeners() {
-        if(config == null) {
-            // We are still initialising
-            return;
-        }
-
-        if (config.getInt("tpa.wait", 0) > 0) {
-            log("PlayerMoveEvent Listener registered.", "Debug");
-            getServer().getPluginManager().registerEvents(new PlayerMoveListener(this), this);
-        } else {
-            log("PlayerMoveEvent Listener unregistered.", "Debug");
-            PlayerMoveEvent.getHandlerList().unregister(this);
-        }
-    }
-
 
     /**
      * Returns the translation for the given key
      * Will return the key if there is no translation in the config
-     * @param key
-     * @return
      */
     public String translate(String key) {
         return this.config.getString(key, key);
@@ -160,21 +173,21 @@ public class Plugin extends JavaPlugin {
     public String translate(String key, Map<String, String> placeholders) {
         String output = this.translate(key);
 
-        if(output.equals(key)) {
+        if (output.equals(key)) {
             // We got a missing key, no need to replace placeholders.
             return output;
         }
 
         for (Map.Entry<String, String> entry : placeholders.entrySet()) {
-            output = output.replaceAll( "%" + entry.getKey() + "%", entry.getValue());
+            output = output.replaceAll("%" + entry.getKey() + "%", entry.getValue());
         }
 
         return output;
     }
 
     public void log(String message, String prefix) {
-        if(prefix != null) {
-            if(Objects.equals(prefix, "Debug") && !config.getBoolean("tpa.verbose")) {
+        if (prefix != null) {
+            if (Objects.equals(prefix, "Debug") && !config.getBoolean("tpa.verbose")) {
                 return;
             }
 
